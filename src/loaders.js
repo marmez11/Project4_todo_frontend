181cbd3f74a9037663d77569fcf71bd978f286d1
marmez11 <116234002+marmez11@users.noreply.github.com>
2023-02-27 00:59:40 -0800

    Add files via upload

---
+++ src/loaders.js
@@ -0,0 +1,16 @@
+// YOUR DEPLOYED API BASE URL
+const URL = "https://xxxxxxx.onrender.com"
+
+//indexLoader => get all todos for index route
+export const indexLoader = async () => {
+    const response = await fetch(URL + "/todos/")
+    const todos = await response.json()
+    return todos
+}
+
+//showLoader => get a single todo for Show route
+export const showLoader = async ({params}) => {
+    const response = await fetch(URL + `/todos/${params.id}/`)
+    const todo = await response.json()
+    return todo
+}
